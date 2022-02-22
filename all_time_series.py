886d9c8b0caba2e3d3d1001d6108c4d2e291ffdd
Semih Berkay Öztürk <90466553+schruteman1881@users.noreply.github.com>
2022-02-22 21:33:52 +0300

    Add files via upload

---
+++ all_time_series.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+r = float(input("What's the minimum rate that you're looking for ?"))
+
+url = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
+response = requests.get(url)
+
+html_content = response.content
+
+soup = BeautifulSoup(html_content,"html.parser")
+
+series = soup.find_all("td",{"class":"titleColumn"})
+rates = soup.find_all("td",{"class":"ratingColumn imdbRating"})
+
+for serie,rate in zip(series,rates):
+    serie = serie.text
+    rate = rate.text
+
+    serie = serie.strip()
+    serie = serie.replace("\n","")
+
+    rate = rate.strip()
+    rate = rate.replace("\n","")
+
+    if float(rate)>r:
+        print(f"Serie:{serie} Rate:{rate}")
+    
